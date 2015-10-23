0d3606dc27794bbc6c15728667a92c1c4367bf7e
Joechen1210 <joes9630003@gmail.com>
2015-10-23 10:56:08 +0800

    test
    test

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -65,8 +65,8 @@
       //}
     　var el2 = document.createElement('div');
        el2.className = 'vjs-overlay';
-    el2.id = 'overlay2';
-      overlay.el += el2;
+       //el2.id = 'overlay2';
+      // overlay.el += el2;
        el2.className += ' vjs-overlay-center';
        el2.innerHTML = 'Hello World';
        player.el().appendChild(el2);
