ad6a47a5616e75c0f9738511fa9e2faea2ba20e0
Joechen1210 <joes9630003@gmail.com>
2015-10-26 16:46:18 +0800

    Update videojs-overlay.js

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -47,42 +47,43 @@
     //  el.appendChild(content);
       
        var el2 = document.createElement('div');
-          //el2.className = 'vjs-overlay';
-          //el2.className += ' vjs-overlay-center';
+          el2.className = 'vjs-overlay';
+          el2.className += ' vjs-overlay-center';
           el2.id = 'overlay2';
           el2.margin = 'top';
       // overlay.el += el2;
     var el2frame = document.createElement('iframe');
        el2frame.src = 'http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html';
        //el2frame.backgroundColor = 'white';
-      // el2frame.className = 'vjs-overlay';
-       //el2frame.className += ' frame';
+       el2frame.className = 'vjs-overlay';
+       el2frame.className += ' frame';
        el2.appendChild(el2frame);
         // el2.innerHTML = '<iframe src="http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html" background-color="white"></iframe>';
          //el2..dialog();
       
       var closediv = document.createElement('div');
-          //closediv.className = 'vjs-overlay';
-          //closediv.className += ' closediv';
+          closediv.className = 'vjs-overlay';
+          closediv.className += ' closediv';
           closediv.id = 'divclose';
       var closebtn = document.createElement('button');
       var closetext = document.createTextNode('X');
       closebtn.appendChild(closetext);
       closebtn.id = 'closeevent';
       closebtn.verticalAlign = 'Top';
+      closediv.appendChild(closebtn);
       //closediv.appendChild(closebtn);
       //closediv.appendChild(el2);
       
-     var parentdiv = document.createElement('div');
-        parentdiv.className = 'vjs-overlay';
-        parentdiv.className += ' vjs-overlay-center';
-        parentdiv.id = 'parentdiv';
+     //var parentdiv = document.createElement('div');
+    //    parentdiv.className = 'vjs-overlay';
+    //    parentdiv.className += ' vjs-overlay-center';
+    //    parentdiv.id = 'parentdiv';
         
-    var divparent = document.getElementById('parentdiv');
-     divparent.appendChild(closediv);
-     divparent.appendChild(el2);
+    //var divparent = document.getElementById('parentdiv');
+    // divparent.appendChild(closediv);
+    // divparent.appendChild(el2);
       
-     closediv.appendChild(closebtn);
+     
       
     // add the overlay to the player
     player.el().appendChild(el);
@@ -96,15 +97,15 @@
        {
          ishidden = false;
         player.el().removeChild(el2);
-        //player.el().removeChild(closediv);
-        player.el().removeChild(parentdiv);
+        player.el().removeChild(closediv);
+        //player.el().removeChild(parentdiv);
          player.play();
        }
        else
        {
        player.el().appendChild(el2);
-      //player.el().appendChild(closediv);
-        player.el().appendChild(parentdiv);
+      player.el().appendChild(closediv);
+      //  player.el().appendChild(parentdiv);
          ishidden = true;
         //window.open ("raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html", "newwindow", height=100, width=400, top=0,left=0, toolbar="no", menubar="no", scrollbars="no", resizable="no",location="no",status="no") 
          player.pause();
@@ -112,9 +113,9 @@
        
          closebtn.onclick = function()
       {
-        player.el().removeChild(el2);
-       // player.el().removeChild(closediv);
-       player.el().removeChild(parentdiv);
+      player.el().removeChild(el2);
+       player.el().removeChild(closediv);
+       //player.el().removeChild(parentdiv);
         player.play();
       }
         //alert("hello")                     //window.open("http://www.w3schools.com/tags/tag_div.asp");
