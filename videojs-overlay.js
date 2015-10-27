73b4dae446c0b3f15607e09b07ee7f472dd545d9
Joechen1210 <joes9630003@gmail.com>
2015-10-27 14:49:17 +0800

    Update videojs-overlay.js

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -70,12 +70,22 @@
           rowdiv2.className += ' row';
           rowdiv2.id = 'row2div';
           
+      var celldiv1 = document.createElement('div');
+          celldiv1.className = 'vjs-overlay';
+          celldiv1.className += ' cell';
+          celldiv1.id = 'cell1div';
+       
+      var celldiv2 = document.createElement('div');
+          celldiv2.className = 'vjs-overlay';
+          celldiv2.className += ' cell';
+          celldiv2.id = 'cell2div';
+          
       var el2 = document.createElement('div');
           el2.className = 'vjs-overlay';
           el2.className += ' vjs-overlay-center';
           el2.id = 'overlay2';
-		  
-		   //add iframe witch use to embed URL 
+		 
+   //add iframe witch use to embed URL 
     var el2frame = document.createElement('iframe');
        el2frame.src = 'http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html';
        el2.appendChild(el2frame);
@@ -91,9 +101,14 @@
       closediv.appendChild(closebtn);
         //rowdiv1.appendChild(closediv);
         //rowdiv2.appendChild(el2);
-        //parentdiv.appendChild(rowdiv1);
-        //parentdiv.appendChild(rowdiv2);
-        parentdiv.appendChild(closediv);
+        rowdiv1.appendChild(celldiv1);
+        rowdiv1.appendChild(closediv);
+        rowdiv2.appendChild(el2);
+        rowdiv2.appendChild(celldiv2);
+        
+        parentdiv.appendChild(rowdiv1);
+        parentdiv.appendChild(rowdiv2);
+        //parentdiv.appendChild(closediv);
         //parentdiv.appendChild(el2);
         
       var ishidden = false;
