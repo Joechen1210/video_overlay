1d5533012fc060428d4dc8e731bcdf2b70a6ea8d
Joechen1210 <joes9630003@gmail.com>
2015-10-30 16:17:56 +0800

    Update videojs-overlay.js

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -104,7 +104,7 @@
       
         parentdiv.appendChild(closebtn);
         maindiv.appendChild(headerdiv);
-        maindiv.appendChild(bodydiv);
+        //maindiv.appendChild(bodydiv);
         //maindiv.appendChild(footerdiv);
         parentdiv.appendChild(maindiv);
         
