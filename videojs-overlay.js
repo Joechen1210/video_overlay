a91a5005223f26390c98f6206cb11a7f067c2447
Joechen1210 <joes9630003@gmail.com>
2015-10-30 16:35:16 +0800

    Update videojs-overlay.js

--- videojs-overlay.js
+++ videojs-overlay.js
@@ -77,7 +77,7 @@
      	 bodydiv.className = 'vjs-overlay';
          bodydiv.className += ' model-body';
          bodydiv.id = 'divbody';
-        bodydiv.innerHTML = "http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html";
+       // bodydiv.innerHTML = "http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html";
         // bodydiv.innerHTML = '<h2>HelloWorld</h2>';
     // var el2 = document.createElement('div');
       //    el2.className = 'vjs-overlay';
@@ -89,7 +89,7 @@
         el2frame.src = 'http://htmlpreview.github.io/?https://raw.githubusercontent.com/Joechen1210/video_overlay/master/index.html';
         //el2frame.className = 'vjs-overlay';
         //el2frame.className += ' iframe';
-        //bodydiv.appendChild(el2frame);
+        bodydiv.appendChild(el2frame);
        
        
       //add close web button
